caad8cac98ea6f874f37db3c0a5c57584d39bac3
matias_gomez <mfgomez.810@gmail.com>
2021-10-17 23:36:49 -0300

    add bootstrap add node-sass

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,8 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<nav className="nav bg-primary">Test</nav>
   );
 }
 
